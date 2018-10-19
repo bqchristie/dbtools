7ed7c950b97de85d99b10d04de732f6f8fd69a7f
Bruce Christie <bruce.christie@alchemysystems.com>
2018-10-19 11:34:45 -0400

    ok

---
+++ server.js
@@ -0,0 +1,16 @@
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var routes = require('./routes');
+var config = require('./config/config')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+var port = process.env.PORT || config.port;        // set our port
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
