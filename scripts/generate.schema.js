b07a1e9bdc48b65d90438774694a09dd5a2bac10
Bruce Christie <bruce.christie@alchemysystems.com>
2018-12-03 15:59:23 -0500

    cleaning up generator
    -author bqchristie

--- scripts/generate.schema.js
+++ scripts/generate.schema.js
@@ -1,27 +1,14 @@
+let _ = require('lodash');
+let requireDir = require('require-dir');
 let DBUtil = require('../db/dbutil');
-let User = require('../model/user');
-let Role = require('../model/role');
-let Permission = require('../model/permission');
-let RolePermission = require('../model/role.permission');
-let Product = require('../model/product');
-let ProductCategory = require('../model/product.category');
-let List = require('../model/list');
-let ListItem = require('../model/list.item');
-let ListShare = require('../model/list.share');
+let model = requireDir('../model');
+let modelNames = _.keys(model).filter(key => !_.startsWith(key,'_'));
+
 
 
 DBUtil.toggleConstraints(false)
     .then(results => {
-        return DBUtil.generateTables([
-            User,
-            Role,
-            Permission,
-            RolePermission,
-            Product,
-            ProductCategory,
-            List,
-            ListItem,
-            ListShare])
+        return DBUtil.generateTables(modelNames.map(modelName=>model[modelName]))
     })
     .then(results => {
         return DBUtil.toggleConstraints(true);
