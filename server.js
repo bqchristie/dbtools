09a638512fb894385d0e13e374ef44d538a72ad4
Bruce <bqchristie@gmail.com>
2018-12-11 20:34:52 -0500

    asa

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+require('dotenv').config()
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var cors  = require('cors');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
 var bodyParser = require('body-parser');
 var routes = require('./routes');
 var config = require('./config/config');
+
+console.log(process.env.ENVIRONMENT);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,9 +19,35 @@ app.use(bodyParser.json());
 app.use(express.static('public'))
 app.use('/api', routes);
 
-var port = process.env.PORT || config.apiPort;        // set our port
+var httpPort = process.env.HTTP_PORT;        // set our port
+var httpsPort = process.env.HTTPS_PORT;        // set our port
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+
+const httpServer = http.createServer(app);
+
+
+httpServer.listen(httpPort, () => {
+    console.log('HTTP Server running on port ' + httpPort);
+});
+
+
+if(httpsPort) {
+    // Certificate
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.fuudlist.com/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/api.fuudlist.com/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/api.fuudlist.com/chain.pem', 'utf8');
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+
+    httpsServer.listen(httpsPort, () => {
+        console.log('HTTPS Server running on port ' + httpsPort);
+    });
+}
